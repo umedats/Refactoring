2f195284d5425fb0c3e9c582d9b79fd164b50558
Tsuneki Umeda <umedat@amazon.co.jp>
2021-10-25 21:01:19 +0900

    use explainingValriable

--- src/refactoring/IntroduceExplainingVariable.java
+++ src/refactoring/IntroduceExplainingVariable.java
@@ -3,8 +3,9 @@ package refactoring;
 public class IntroduceExplainingVariable {
 
     double price(int quantity, int itemPrice) {
-        return quantity * itemPrice
-                - Math.max(0, quantity - 500) * itemPrice * 0.05
-                + Math.min(quantity * itemPrice * 0.1, 100);
+        final double basePrice = quantity * itemPrice;
+        final double quantityDiscount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+        final double shipping = Math.min(basePrice * 0.1, 100);
+        return basePrice - quantityDiscount + shipping;
     }
 }
