2cdfd989198bb266d6a0d40c27c03a4f4353399e
Tsuneki Umeda <umedat@amazon.co.jp>
2021-10-26 21:44:48 +0900

    use temp val for output

--- src/refactoring/RemoveAssignmentToParameters.java
+++ src/refactoring/RemoveAssignmentToParameters.java
@@ -1,17 +1,18 @@
 package refactoring;
 
 public class RemoveAssignmentToParameters {
-    int discount(int inputVal, int quantity, int yearToDate) {
+    int discount(final int inputVal, final int quantity, final int yearToDate) {
+        int result = inputVal;
         if (inputVal > 50) {
-            inputVal -= 2;
+            result -= 2;
         }
         if (quantity > 100) {
-            inputVal -= 1;
+            result -= 1;
         }
         if (yearToDate > 10000) {
-            inputVal -= 4;
+            result -= 4;
         }
-        return inputVal;
+        return result;
     }
 
 
